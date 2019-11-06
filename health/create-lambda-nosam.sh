060ddd9ce65731d17caeab0ea4eb21424e7c8872
Edge <edgema@88e9fe5e4195.ant.amazon.com>
2019-11-06 11:23:56 +0800

    added health check

---
+++ health/create-lambda-nosam.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# or in the "license" file accompanying this file. This file is distributed 
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+# express or implied. See the License for the specific language governing 
+# permissions and limitations under the License.
+
+# Uses SAM (serverless application model) to deploy the peer health check Lambda function
+
+if [ -z "$NETWORKID" ]
+then
+      echo "Environment variables \$NETWORKID, \$MEMBERID or \$REGION are empty. Please see the pre-requisites in the README"
+fi
+
+if [ -z "$NETWORKNAME" ]
+then
+      echo "Environment variables \$NETWORKID, \$MEMBERID or \$REGION are empty. Please see the pre-requisites in the README"
+fi
+
+if [ -z "$SNSEMAIL" ]
+then
+      echo "\$SNSEMAIL is empty. Please see the pre-requisites in the README"
+fi
+
+echo Build the Lambda function and copy to S3
+aws s3 mb s3://$NETWORKID-peer-health --region $REGION  
+mkdir -p ./build
+cp -R peer-health tmp
+. ~/.nvm/nvm.sh
+nvm use lts/carbon
+cd build
+npm install
+aws s3 cp . s3://$NETWORKID-peer-health --region $REGION --recursive
+
+echo Deploy the Lambda function
+aws cloudformation deploy --template-file peer-health-template.yaml --region $REGION --capabilities CAPABILITY_IAM \
+--stack-name $NETWORKNAME-peer-health-lambda \
+--parameter-overrides NetworkId=$NETWORKID MemberId=$MEMBERID NotificationEmail=$SNSEMAIL

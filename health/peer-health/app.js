0afa4e31f3bee4cebcb364e087264b5362d7e510
Edge <edgema@88e9fe5e4195.ant.amazon.com>
2019-11-15 14:31:55 +0800

    added health check

--- health/peer-health/app.js
+++ health/peer-health/app.js
@@ -139,7 +139,7 @@ exports.handler = async (event) => {
                 // available or not
                 let cwAlarmParams = {
                     AlarmName: 'NodeAvailability-' + node.Id,
-                    ComparisonOperator: ' LessThanThreshold',
+                    ComparisonOperator: 'LessThanThreshold',
                     EvaluationPeriods: '1',
                     AlarmDescription: 'Alarm if managed blockchain peer node becomes UNAVAILABLE',
                     Dimensions: [

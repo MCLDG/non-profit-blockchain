b07b67b8c4ecb34cf1d22c49fb3763c6a0ff614d
Edge <edgema@88e9fe5e4195.ant.amazon.com>
2020-03-17 16:32:53 +0800

    more reliable invoke and query

--- ngo-rest-api/invoke.js
+++ ngo-rest-api/invoke.js
@@ -18,18 +18,34 @@
 var util = require('util');
 var helper = require('./connection.js');
 var logger = helper.getLogger('Invoke');
+var channel = null;
 
-var invokeChaincode = async function(peerNames, channelName, chaincodeName, args, fcn, username, orgName) {
+//Have to use this wrapper to properly handle rejection error from channel.sendTransactionProposal
+function __sendTransactionProposal(channel, request, timeout) {
+	const fcnName = "[invokeChaincode __sendTransactionProposal]";
+
+	return new Promise((resolve, reject) => {
+		channel.sendTransactionProposal(request, timeout)
+			.then((results) => {
+				resolve(results);
+			})
+			.catch((err) => {
+				throw new Error('${fcnName} ${err}');
+			});
+	});
+}
+
+var invokeChaincode = async function (peerNames, channelName, chaincodeName, args, fcn, userName, orgName) {
 	logger.info(util.format('\n============ invokeChaincode - chaincode %s, function %s, on the channel \'%s\' for org: %s\n',
 		chaincodeName, fcn, channelName, orgName));
 	var error_message = null;
 	var txIdAsString = null;
 	try {
 		// first setup the client for this org
-		var client = await helper.getClientForOrg(orgName, username);
+		var client = await helper.getClientForOrg(orgName, userName);
 		logger.info('##### invokeChaincode - Successfully got the fabric client for the organization "%s"', orgName);
-		var channel = client.getChannel(channelName);
-		if(!channel) {
+		if (!channel) channel = client.getChannel(channelName);
+		if (!channel) {
 			let message = util.format('##### invokeChaincode - Channel %s was not defined in the connection profile', channelName);
 			logger.error(message);
 			throw new Error(message);
@@ -47,8 +63,20 @@ var invokeChaincode = async function(peerNames, channelName, chaincodeName, args
 			txId: txId
 		};
 
-		logger.info('##### invokeChaincode - Invoke transaction request to Fabric %s', JSON.stringify(request));
-		let results = await channel.sendTransactionProposal(request);
+		logger.info('##### invokeChaincode - txId: ' + txIdAsString + '. Invoke transaction request to Fabric %s', JSON.stringify(request));
+		let results;
+		try {
+			results = await __sendTransactionProposal(channel, request, 30000);
+		} catch (err) {
+			logger.error('##### invokeChaincode - txId: ' + txIdAsString + '. got error while sending transaction proposal:' + err);
+			logger.error('##### invokeChaincode - txId: ' + txIdAsString + '. retrying one more time before throwing an error');
+			results = await __sendTransactionProposal(channel, request, 30000);
+		}
+
+		if (!results) {
+			logger.error('##### invokeChaincode - txId: ' + txIdAsString + '. Unable to obtain transaction proposal for txId: ' + txIdAsString);
+			throw new Error('##### invokeChaincode - txId: ' + txIdAsString + '. Unable to obtain transaction proposal for txId: ' + txIdAsString);
+		}
 
 		// the returned object has both the endorsement results
 		// and the actual proposal, the proposal will be needed
@@ -56,126 +84,153 @@ var invokeChaincode = async function(peerNames, channelName, chaincodeName, args
 		var proposalResponses = results[0];
 		var proposal = results[1];
 
-		// lets have a look at the responses to see if they are
-		// all good, if good they will also include signatures
-		// required to be committed
-		var successfulResponses = true;
-		for (var i in proposalResponses) {
-			let oneSuccessfulResponse = false;
-			if (proposalResponses && proposalResponses[i].response &&
-				proposalResponses[i].response.status === 200) {
-				oneSuccessfulResponse = true;
-				logger.info('##### invokeChaincode - received successful proposal response');
-			} else {
-				logger.error('##### invokeChaincode - received unsuccessful proposal response');
-			}
-			successfulResponses = successfulResponses & oneSuccessfulResponse;
+		if (!proposalResponses.length) {
+			logger.error('##### invokeChaincode - txId: ' + txIdAsString + '. proposalResponses is empty: No results were returned from the request');
+			throw new Error('##### invokeChaincode - txId: ' + txIdAsString + '. No results were returned from the sendTransactionProposal request');
 		}
 
-		if (successfulResponses) {
-			logger.info(util.format(
-				'##### invokeChaincode - Successfully sent Proposal and received ProposalResponse: Status - %s, message - "%s"',
-				proposalResponses[0].response.status, proposalResponses[0].response.message));
+		logger.info(util.format(
+			'##### invokeChaincode - txId: ' + txIdAsString + '. Successfully sent Proposal and received ProposalResponse: Status - %s, message - "%s"',
+			util.inspect(proposalResponses)));
 
-			// wait for the channel-based event hub to tell us
-			// that the commit was good or bad on each peer in our organization
-			var promises = [];
-			let event_hubs = channel.getChannelEventHubsForOrg();
-			event_hubs.forEach((eh) => {
-				logger.info('##### invokeChaincode - invokeEventPromise - setting up event handler');
-				let invokeEventPromise = new Promise((resolve, reject) => {
-					let event_timeout = setTimeout(() => {
-						let message = 'REQUEST_TIMEOUT:' + eh.getPeerAddr();
-						logger.error(message);
-						eh.disconnect();
-					}, 10000);
-					eh.registerTxEvent(txIdAsString, (tx, code, block_num) => {
-						logger.info('##### invokeChaincode - The invoke chaincode transaction has been committed on peer %s',eh.getPeerAddr());
-						logger.info('##### invokeChaincode - Transaction %s has status of %s in block %s', tx, code, block_num);
+		const validResponses = [];
+		const errorResponses = [];
+
+		proposalResponses.forEach((responseContent) => {
+			if (responseContent instanceof Error) {
+				logger.warn('##### invokeChaincode - txId: ' + txIdAsString + '.  Received error response from peer:', responseContent);
+				errorResponses.push(responseContent);
+			} else {
+				logger.debug('##### invokeChaincode - txId: ' + txIdAsString + '. valid response from peer %j', responseContent.peer);
+				validResponses.push(responseContent);
+			}
+		});
+
+		if (validResponses.length === 0) {
+			const errorMessages = errorResponses.map((response) => util.format('peer=%s, status=%s, message=%s',
+				response.peer.name, response.status, response.message));
+			const messages = Array.of('No valid responses from any peers. ${errorResponses.length} peer error responses:',
+				...errorMessages);
+			const msg = messages.join('\n    ');
+			logger.error('##### invokeChaincode - txId: ' + txIdAsString + '.  ' + msg);
+			throw new Error(msg);
+		}
+
+		logger.info(util.format(
+			'##### invokeChaincode - txId: ' + txIdAsString + '. Successfully sent Proposal and received ProposalResponse: Status - %s, message - "%s"',
+			validResponses[0].response.status, validResponses[0].response.message));
+
+		// wait for the channel-based event hub to tell us
+		// that the commit was good or bad on each peer in our organization
+		var promises = [];
+		let event_hubs = channel.getChannelEventHubsForOrg();
+		event_hubs.forEach((eh) => {
+			logger.info('##### invokeChaincode - txId: ' + txIdAsString + '. invokeEventPromise - setting up event handler');
+			let invokeEventPromise = new Promise((resolve, reject) => {
+				let timer = true;
+				let maxRetries = 10;
+				let event_timeout = setTimeout(() => {
+					let message = '##### invokeChaincode - txId: ' + txIdAsString + '. REQUEST_TIMEOUT:' + eh.getPeerAddr();
+					logger.error(message);
+					timer = false;
+					eh.disconnect();
+				}, 10000);
+				eh.registerTxEvent(txIdAsString, (tx, code, block_num) => {
+						logger.info('##### invokeChaincode - txId: ' + txIdAsString + '. The invoke chaincode transaction has been committed on peer %s', eh.getPeerAddr());
+						logger.info('##### invokeChaincode - txId: ' + txIdAsString + '. Transaction %s has status of %s in block %s', tx, code, block_num);
 						clearTimeout(event_timeout);
 
 						if (code !== 'VALID') {
-							let message = util.format('##### invokeChaincode - The invoke chaincode transaction was invalid, code:%s',code);
+							let message = util.format('##### invokeChaincode - txId: ' + txIdAsString + '. The invoke chaincode transaction was invalid, code:%s', code);
 							logger.error(message);
 							reject(new Error(message));
 						} else {
-							let message = '##### invokeChaincode - The invoke chaincode transaction was valid.';
+							let message = '##### invokeChaincode - txId: ' + txIdAsString + '. The invoke chaincode transaction was valid.';
 							logger.info(message);
 							resolve(message);
 						}
 					}, (err) => {
+						if (eh.isconnected()) {
+							logger.error('##### invokeChaincode - txId: ' + txIdAsString + '. Failed to receive the block event: ' + err);
+							logger.info('##### invokeChaincode - txId: ' + txIdAsString + '. Event hub connected. Peer name: ' + eh.getPeerAddr() + ' Channel name: ' + channel.getName());
+							clearTimeout(event_timeout);
+							reject('##### invokeChaincode - txId: ' + txIdAsString + '. Failed to receive the block event: ' + err);
+						} else {
+							// Trying to reconnect until we run out of time in the main timer 'event_timeout' 
+							// or we reach maximum number of retries
+							if (timer && (maxRetries >= 0)) {
+								maxRetries -= 1;
+								eh.checkConnection(true);
+								logger.debug('##### invokeChaincode - txId: ' + txIdAsString + '. Retrying Event Hub connection for txId ' + txIdAsString);
+							} else {
+								logger.error('##### invokeChaincode - txId: ' + txIdAsString + '. Seems like we ran out of time for Event Hub timeout or reached max number of retries: ' + maxRetries);
+								reject('##### invokeChaincode - txId: ' + txIdAsString + '. Seems like we ran out of time for Event Hub timeout or reached max number of retries: ' + maxRetries);
+							}
+						}
 						clearTimeout(event_timeout);
-						logger.error(err);
+						logger.error('##### invokeChaincode - txId: ' + txIdAsString + '. in Event Hub - Error in event hub listener registerTxEvent ' + err);
 						reject(err);
 					},
-						// the default for 'unregister' is true for transaction listeners
-						// so no real need to set here, however for 'disconnect'
-						// the default is false as most event hubs are long running
-						// in this use case we are using it only once
-						{unregister: true, disconnect: true}
-					);
-					eh.connect();
-				});
-				promises.push(invokeEventPromise);
+					// the default for 'unregister' is true for transaction listeners
+					// so no real need to set here, however for 'disconnect'
+					// the default is false as most event hubs are long running
+					// in this use case we are using it only once
+					{
+						unregister: true,
+						disconnect: false
+					}
+				);
+				eh.connect();
 			});
+			promises.push(invokeEventPromise);
+		});
 
-			var orderer_request = {
-				txId: txId,
-				proposalResponses: proposalResponses,
-				proposal: proposal
-			};
-			var sendPromise = channel.sendTransaction(orderer_request);
-			// put the send to the ordering service last so that the events get registered and
-			// are ready for the orderering and committing
-			promises.push(sendPromise);
-			let results = await Promise.all(promises);
-			logger.info(util.format('##### invokeChaincode ------->>> R E S P O N S E : %j', results));
-			let response = results.pop(); //  ordering service results are last in the results
-			if (response.status === 'SUCCESS') {
-				logger.info('##### invokeChaincode - Successfully sent transaction to the ordering service.');
-			} else {
-				error_message = util.format('##### invokeChaincode - Failed to order the transaction. Error code: %s',response.status);
-				logger.info(error_message);
-			}
-
-			// now see what each of the event hubs reported
-			for(let i in results) {
-				let event_hub_result = results[i];
-				let event_hub = event_hubs[i];
-				logger.info('##### invokeChaincode - Event results for event hub :%s',event_hub.getPeerAddr());
-				if(typeof event_hub_result === 'string') {
-					logger.info('##### invokeChaincode - ' + event_hub_result);
-				} 
-				else {
-					if (!error_message) error_message = event_hub_result.toString();
-					logger.info('##### invokeChaincode - ' + event_hub_result.toString());
-				}
-			}
-		} 
-		else {
-			error_message = util.format('##### invokeChaincode - Failed to send Proposal and receive all good ProposalResponse. Status code: ' + 
-				proposalResponses[0].status + ', ' + 
-				proposalResponses[0].message + '\n' +  
-				proposalResponses[0].stack);
+		var orderer_request = {
+			txId: txId,
+			proposalResponses: validResponses,
+			proposal: proposal
+		};
+		var sendPromise = channel.sendTransaction(orderer_request, 30000);
+		// put the send to the ordering service last so that the events get registered and
+		// are ready for the orderering and committing
+		promises.push(sendPromise);
+		results = await Promise.all(promises);
+		logger.info(util.format('##### invokeChaincode - txId: ' + txIdAsString + '.  ------->>> R E S P O N S E : %j', results));
+		let response = results.pop(); //  ordering service results are last in the results
+		if (response.status === 'SUCCESS') {
+			logger.info('##### invokeChaincode - txId: ' + txIdAsString + '. Successfully sent transaction to the ordering service.');
+		} else {
+			error_message = util.format('##### invokeChaincode - txId: ' + txIdAsString + '. Failed to order the transaction. Error code: %s', response.status);
 			logger.info(error_message);
 		}
-	} 
-	catch (error) {
-		logger.error('##### invokeChaincode - Failed to invoke due to error: ' + error.stack ? error.stack : error);
+
+		// now see what each of the event hubs reported
+		for (let i in results) {
+			let event_hub_result = results[i];
+			let event_hub = event_hubs[i];
+			logger.info('##### invokeChaincode - txId: ' + txIdAsString + '. Event results for event hub :%s', event_hub.getPeerAddr());
+			if (typeof event_hub_result === 'string') {
+				logger.info('##### invokeChaincode - txId: ' + txIdAsString + '. ' + event_hub_result);
+			} else {
+				if (!error_message) error_message = event_hub_result.toString();
+				logger.info('##### invokeChaincode - txId: ' + txIdAsString + '. ' + event_hub_result.toString());
+			}
+		}
+	} catch (error) {
+		logger.error('##### invokeChaincode - txId: ' + txIdAsString + '. Failed to invoke due to error: ' + error.stack ? error.stack : error);
 		error_message = error.toString();
 	}
 
 	if (!error_message) {
 		let message = util.format(
-			'##### invokeChaincode - Successfully invoked chaincode %s, function %s, on the channel \'%s\' for org: %s and transaction ID: %s',
+			'##### invokeChaincode - txId: ' + txIdAsString + '. Successfully invoked chaincode %s, function %s, on the channel \'%s\' for org: %s and transaction ID: %s',
 			chaincodeName, fcn, channelName, orgName, txIdAsString);
 		logger.info(message);
 		let response = {};
 		response.transactionId = txIdAsString;
 		return response;
-	} 
-	else {
-		let message = util.format('##### invokeChaincode - Failed to invoke chaincode. cause:%s', error_message);
+	} else {
+		let message = util.format('##### invokeChaincode - txId: ' + txIdAsString + '. Failed to invoke chaincode. cause:%s', error_message);
 		logger.error(message);
 		throw new Error(message);
 	}
--- ngo-rest-api/query.js
+++ ngo-rest-api/query.js
@@ -17,70 +17,256 @@ var util = require('util');
 var helper = require('./connection.js');
 var logger = helper.getLogger('Query');
 
-var queryChaincode = async function(peers, channelName, chaincodeName, args, fcn, username, orgName) {
+var channel = null;
+
+var queryChaincode = async function(peers, channelName, chaincodeName, args, fcn, userName, orgName) {
 	try {
 		// setup the client for this org
-		var client = await helper.getClientForOrg(orgName, username);
-		logger.info('============ START queryChaincode - Successfully got the fabric client for the organization "%s"', orgName);
-		var channel = client.getChannel(channelName);
-		if(!channel) {
-			let message = util.format('##### queryChaincode - Channel %s was not defined in the connection profile', channelName);
+		var client = await helper.getClientForOrg(orgName, userName);
+		logger.info('============ START queryChaincode for fcn: ' + fcn + '.- Successfully got the fabric client for the organization "%s"', orgName);
+		if (!channel) channel = client.getChannel(channelName);
+		if (!channel) {
+			let message = util.format('##### queryChaincode - fcn: ' + fcn + '. Channel %s was not defined in the connection profile', channelName);
 			logger.error(message);
 			throw new Error(message);
 		}
 
 		// send query
 		var request = {
-			targets : peers, 
+			targets: peers,
 			chaincodeId: chaincodeName,
 			fcn: fcn,
 			args: [JSON.stringify(args)]
 		};
 
-		logger.info('##### queryChaincode - Query request to Fabric %s', JSON.stringify(request));
+		logger.info('##### queryChaincode - fcn: ' + fcn + '. Query request to Fabric %s', JSON.stringify(request));
 		let responses = await channel.queryByChaincode(request);
-        let ret = [];
-		if (responses) {
-            // you may receive multiple responses if you passed in multiple peers. For example,
-            // if the targets : peers in the request above contained 2 peers, you should get 2 responses
-			for (let i = 0; i < responses.length; i++) {
-                logger.info('##### queryChaincode - result of query: ' + responses[i].toString('utf8') + '\n');
+
+		if (!responses) {
+			logger.error(`##### queryChaincode - Unable to queryByChaincode for fcn: ${fcn}. No responses returned from peers`);
+			throw new Error(`##### queryChaincode - Unable to queryByChaincode for fcn: ${fcn}. No responses returned from peers`);
+		}
+
+		for (let i = 0; i < responses.length; i++) {
+			logger.info(util.format('Query result from peer [%s]: %s', i, responses[i].toString('utf8')));
+		}
+		
+		const validResponses = [];
+		const errorResponses = [];
+
+		responses.forEach((responseContent) => {
+			if (responseContent instanceof Error || responseContent.toString('utf8').indexOf("Error: transaction returned with failure") != -1 || responseContent.toString('utf8').indexOf("Error: failed to execute transaction") != -1) {
+				logger.warn('##### queryChaincode - fcn: ' + fcn + '.  Received error response from peer:', responseContent);
+				errorResponses.push(responseContent);
 			}
-			// check for error
-			let response = responses[0].toString('utf8');
-			logger.info('##### queryChaincode - type of response: %s', typeof response);
-			if (responses[0].toString('utf8').indexOf("Error: transaction returned with failure") != -1) {
-				let message = util.format('##### queryChaincode - error in query result: %s', responses[0].toString('utf8'));
-				logger.error(message);
-				throw new Error(message);	
+			else {
+				logger.debug('##### queryChaincode - fcn: ' + fcn + '. valid response from peer %s', responseContent);
+				validResponses.push(responseContent);
 			}
-            // we will only use the first response. We strip out the Fabric key and just return the payload
-            let json = JSON.parse(responses[0].toString('utf8'));
-			logger.info('##### queryChaincode - Query json %s', util.inspect(json));
-			if (Array.isArray(json)) {
+		});
+
+		if (validResponses.length === 0) {
+			const errorMessages = errorResponses.map((response) => util.format('peer=%s, status=%s, message=%s',
+				response.peer.name, response.status, response.message));
+			const messages = Array.of(`No valid responses from any peers. ${errorResponses.length} peer error responses:`,
+				...errorMessages);
+			const msg = messages.join('\n    ');
+			logger.error('##### queryChaincode - fcn: ' + fcn + '.  ' + msg);
+			throw new Error(msg);
+		}
+
+		let ret = [];
+		// We will only use the first response. If we are querying history, we return the entire payload as it
+		// contains useful info such as the transaction ID. Otherwise we strip out the Fabric key and just return the payload
+		let json = JSON.parse(validResponses[0].toString('utf8'));
+		logger.info('##### queryChaincode - fcn: ' + fcn + '. Valid response as JSON %s', util.inspect(json));
+		if (Array.isArray(json)) {
+			if (fcn == 'queryHistoryForKey') {
+				ret.push(json);
+			}
+			else {
 				for (let key in json) {
 					if (json[key]['Record']) {
-						ret.push(json[key]['Record']); 
-					} 
+						ret.push(json[key]['Record']);
+					}
 					else {
-						ret.push(json[key]); 
+						ret.push(json[key]);
 					}
 				}
 			}
-			else {
-				ret.push(json); 
-			}
- 			return ret;
-		} 
-		else {
-			logger.error('##### queryChaincode - result of query, responses is null');
-			return 'responses is null';
 		}
-	} 
-	catch(error) {
-		logger.error('##### queryChaincode - Failed to query due to error: ' + error.stack ? error.stack : error);
+		else {
+			ret.push(json);
+		}
+		return ret;
+	}
+	catch (error) {
+		logger.error('##### queryChaincode - fcn: ' + fcn + '. Failed to query due to error: ' + error.stack ? error.stack : error);
+		return error.toString();
+	}
+};
+
+var getChannelsForPeer = async function(peer, userName, orgName) {
+	try {
+		// setup the client for this org
+		var client = await helper.getClientForOrg(orgName, userName);
+		logger.info('============ START getChannelsForPeer - Successfully got the fabric client for the organization "%s"', orgName);
+
+		let response_payload = await client.queryChannels(peer);
+		if (response_payload) {
+			logger.debug(response_payload);
+			return response_payload;
+		}
+		else {
+			logger.error('##### getChannelsForPeer - response_payload is null');
+			return 'response_payload is null';
+		}
+	}
+	catch (error) {
+		logger.error('##### getChannelsForPeer - Failed to query due to error: ' + error.stack ? error.stack : error);
+		return error.toString();
+	}
+};
+
+var getChaincodesForPeer = async function(peer, userName, orgName) {
+	try {
+		// setup the client for this org
+		var client = await helper.getClientForOrg(orgName, userName);
+		logger.info('============ START getChaincodesForPeer - Successfully got the fabric client for the organization "%s"', orgName);
+
+		let response_payload = await client.queryInstalledChaincodes(peer);
+		if (response_payload) {
+			logger.debug(response_payload);
+			return response_payload;
+		}
+		else {
+			logger.error('##### getChaincodesForPeer - response_payload is null');
+			return 'response_payload is null';
+		}
+	}
+	catch (error) {
+		logger.error('##### getChaincodesForPeer - Failed to query due to error: ' + error.stack ? error.stack : error);
+		return error.toString();
+	}
+};
+
+var queryChannelInfo = async function(peer, channelName, userName, orgName) {
+	try {
+		// setup the client for this org
+		var client = await helper.getClientForOrg(orgName, userName);
+		logger.info('============ START queryChannelInfo - Successfully got the fabric client for the organization "%s"', orgName);
+		var channel = client.getChannel(channelName);
+		if (!channel) {
+			let message = util.format('##### queryChannelInfo - Channel %s was not defined in the connection profile', channelName);
+			logger.error(message);
+			throw new Error(message);
+		}
+
+
+		let response_payload = await channel.queryInfo(peer);
+		if (response_payload) {
+			logger.debug(response_payload);
+			return response_payload;
+		}
+		else {
+			logger.error('##### queryChannelInfo - response_payload is null');
+			return 'response_payload is null';
+		}
+	}
+	catch (error) {
+		logger.error('##### queryChannelInfo - Failed to query due to error: ' + error.stack ? error.stack : error);
+		return error.toString();
+	}
+};
+
+var getInstantiatedChaincodes = async function(peer, channelName, userName, orgName) {
+	try {
+		// setup the client for this org
+		var client = await helper.getClientForOrg(orgName, userName);
+		logger.info('============ START getInstantiatedChaincodes - Successfully got the fabric client for the organization "%s"', orgName);
+		var channel = client.getChannel(channelName);
+		if (!channel) {
+			let message = util.format('##### getInstantiatedChaincodes - Channel %s was not defined in the connection profile', channelName);
+			logger.error(message);
+			throw new Error(message);
+		}
+
+		let response_payload = await channel.queryInstantiatedChaincodes(peer);
+		if (response_payload) {
+			logger.debug(response_payload);
+			return response_payload;
+		}
+		else {
+			logger.error('##### getInstantiatedChaincodes - response_payload is null');
+			return 'response_payload is null';
+		}
+	}
+	catch (error) {
+		logger.error('##### getInstantiatedChaincodes - Failed to query due to error: ' + error.stack ? error.stack : error);
+		return error.toString();
+	}
+};
+
+var getBlockByNumber = async function(peer, channelName, blockId, userName, orgName) {
+	try {
+		// setup the client for this org
+		var client = await helper.getClientForOrg(orgName, userName);
+		logger.info('============ START getBlockByNumber - Successfully got the fabric client for the organization "%s"', orgName);
+		var channel = client.getChannel(channelName);
+		if (!channel) {
+			let message = util.format('##### getBlockByNumber - Channel %s was not defined in the connection profile', channelName);
+			logger.error(message);
+			throw new Error(message);
+		}
+
+		let response_payload = await channel.queryBlock(parseInt(blockId), peer);
+		if (response_payload) {
+			logger.debug(response_payload);
+			return response_payload;
+		}
+		else {
+			logger.error('##### getBlockByNumber - response_payload is null');
+			return 'response_payload is null';
+		}
+	}
+	catch (error) {
+		logger.error('##### getBlockByNumber - Failed to query due to error: ' + error.stack ? error.stack : error);
+		return error.toString();
+	}
+};
+
+var getTransactionById = async function(peer, channelName, transactionId, userName, orgName) {
+	try {
+		// setup the client for this org
+		var client = await helper.getClientForOrg(orgName, userName);
+		logger.info('============ START getTransactionById - Successfully got the fabric client for the organization "%s"', orgName);
+		var channel = client.getChannel(channelName);
+		if (!channel) {
+			let message = util.format('##### getTransactionById - Channel %s was not defined in the connection profile', channelName);
+			logger.error(message);
+			throw new Error(message);
+		}
+
+		let response_payload = await channel.queryTransaction(transactionId, peer);
+		if (response_payload) {
+			logger.debug(response_payload);
+			return response_payload;
+		}
+		else {
+			logger.error('##### getTransactionById - response_payload is null');
+			return 'response_payload is null';
+		}
+	}
+	catch (error) {
+		logger.error('##### getTransactionById - Failed to query due to error: ' + error.stack ? error.stack : error);
 		return error.toString();
 	}
 };
 
 exports.queryChaincode = queryChaincode;
+exports.getChannelsForPeer = getChannelsForPeer;
+exports.getChaincodesForPeer = getChaincodesForPeer;
+exports.queryChannelInfo = queryChannelInfo;
+exports.getInstantiatedChaincodes = getInstantiatedChaincodes;
+exports.getBlockByNumber = getBlockByNumber;
+exports.getTransactionById = getTransactionById;
